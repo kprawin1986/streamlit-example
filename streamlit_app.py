f993cb13c299093f90d2b2e535a9e63c5116f282
kprawin1986 <37552763+kprawin1986@users.noreply.github.com>
2023-12-22 22:46:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,22 +8,12 @@ from docx import Document
 import frontend_lib
 import google.generativeai as genai
 
-def send_query_to_ai (prompt, API_KEY):
-    try:
-        genai.configure(api_key=API_KEY)
-        model = genai.GenerativeModel('gemini-pro')
-        response = model.generate_content(prompt)
-        return response.text
-    except Exception as e:
-        print(e)
-        return False
-
-def calculate_token_size(text):
-    words = text.split()
-    token_count = len(words)
-    return token_count
-
-
 if __name__ == '__main__':
     #print(frontend_lib.st.session_state)
-             markdown_testplan = send_query_to_ai(prompt, API_KEY)
+   try:
+        bard = BardCookies(cookie_dict=cookie_dict)
+        response = bard.get_answer(prompt)
+        return (response['content'])
+    except:
+        print("Cookie update required")
+        return None
