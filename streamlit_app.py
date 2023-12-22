3a1fcb8b58eea0b0eb4d3860de2fa8a80048d733
kprawin1986 <37552763+kprawin1986@users.noreply.github.com>
2023-12-22 21:51:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.header('st.button')
 
 if st.button('Hi'):
      st.write('Yes. Who are you??')
-     if st.button('Hi'):
+     if st.button('Hii'):
          st.write('I am hearing you')
 else:
      st.write('Goodbye')
