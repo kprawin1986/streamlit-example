71ce7bd1c4a21fc392bb2a91191d300469ca5de8
kprawin1986 <37552763+kprawin1986@users.noreply.github.com>
2023-12-22 22:37:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,29 @@
-import streamlit as st
 
-st.header('st.button')
+import re
+import sys
+import markdown
+from google_gemini_api import send_query_to_ai
+from google_gemini_api import calculate_token_size
+from docx import Document
+import frontend_lib
+import google.generativeai as genai
 
-if st.button('Hi'):
-     st.write('Yes. Who are you??')
-     if st.button('Hii'):
-         st.write('I am hearing you')
-else:
-     st.write('Goodbye')
+def send_query_to_ai (prompt, API_KEY):
+    try:
+        genai.configure(api_key=API_KEY)
+        model = genai.GenerativeModel('gemini-pro')
+        response = model.generate_content(prompt)
+        return response.text
+    except Exception as e:
+        print(e)
+        return False
+
+def calculate_token_size(text):
+    words = text.split()
+    token_count = len(words)
+    return token_count
+
+
+if __name__ == '__main__':
+    #print(frontend_lib.st.session_state)
+             markdown_testplan = send_query_to_ai(prompt, API_KEY)
